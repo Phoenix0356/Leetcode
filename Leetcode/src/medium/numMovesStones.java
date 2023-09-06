5d58d4f1ace6b609bb0c9405dbfc4466062153cf
Phoenix0356 <1242780667@qq.com>
2023-09-06 14:31:11 +0800

    20230906

---
+++ Leetcode/src/medium/numMovesStones.java
@@ -0,0 +1,21 @@
+package medium;
+
+import java.util.Arrays;
+
+public class numMovesStones {
+    public int[] numMovesStones(int a, int b, int c) {
+        int x = Math.min(Math.min(a, b), c);
+        int z = Math.max(Math.max(a, b), c);
+        int y = a + b + c - x - z;
+
+        int[] res = new int[2];
+        res[0] = 2;
+        if (z - y == 1 && y - x == 1) {
+            res[0] = 0;
+        } else if (z - y <= 2 || y - x <= 2) {
+            res[0] = 1;
+        }
+        res[1] = z - x - 2;
+        return res;
+    }
+}
