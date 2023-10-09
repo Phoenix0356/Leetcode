a8bfa89762dd7121fc6f1e2e3d8427814162b9e5
Phoenix0356 <1242780667@qq.com>
2023-10-09 19:15:25 +0800

    202301009

---
+++ Leetcode/src/medium/maxProfit3.java
@@ -0,0 +1,13 @@
+package medium;
+
+public class maxProfit3 {
+    public int maxProfit(int[] prices, int fee) {
+        int hold=-prices[0];
+        int sold=0;
+        for (int i=1;i<prices.length;i++){
+            hold=Math.max(hold,sold-prices[i]);
+            sold=Math.max(sold,hold+prices[i]-fee);
+        }
+        return sold;
+    }
+}
