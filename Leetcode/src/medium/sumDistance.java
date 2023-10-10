37d03f013f2f7962013817642112af99147ef2f1
Phoenix0356 <1242780667@qq.com>
2023-10-10 14:03:27 +0800

    202301010

---
+++ Leetcode/src/medium/sumDistance.java
@@ -0,0 +1,25 @@
+package medium;
+
+import java.util.Arrays;
+
+public class sumDistance {
+    public int sumDistance(int[] nums, String s, int d) {
+        int n = nums.length;
+        long[] pos = new long[n];
+        int MOD=1000000007;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'L') {
+                pos[i] = (long) nums[i] - d;
+            } else {
+                pos[i] = (long) nums[i] + d;
+            }
+        }
+        Arrays.sort(pos);
+        long res = 0;
+        for (int i = 1; i < n; i++) {
+            res += 1L * (pos[i] - pos[i - 1]) * i % MOD * (n - i) % MOD;
+            res %= MOD;
+        }
+        return (int) res;
+    }
+}
