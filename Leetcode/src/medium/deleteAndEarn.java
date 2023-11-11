af75d746f0aac8b0cae916895f2f0662aba15bdd
Phoenix0356 <1242780667@qq.com>
2023-11-11 15:14:34 +0800

    202301111

---
+++ Leetcode/src/medium/deleteAndEarn.java
@@ -0,0 +1,46 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class deleteAndEarn {
+    public int deleteAndEarn(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        Arrays.sort(nums);
+        List<Integer> sum = new ArrayList<Integer>();
+        sum.add(nums[0]);
+        int size = 1;
+        for (int i = 1; i < n; ++i) {
+            int val = nums[i];
+            if (val == nums[i - 1]) {
+                sum.set(size - 1, sum.get(size - 1) + val);
+            } else if (val == nums[i - 1] + 1) {
+                sum.add(val);
+                ++size;
+            } else {
+                ans += rob(sum);
+                sum.clear();
+                sum.add(val);
+                size = 1;
+            }
+        }
+        ans += rob(sum);
+        return ans;
+    }
+
+    public int rob(List<Integer> nums) {
+        int size = nums.size();
+        if (size == 1) {
+            return nums.get(0);
+        }
+        int first = nums.get(0), second = Math.max(nums.get(0), nums.get(1));
+        for (int i = 2; i < size; i++) {
+            int temp = second;
+            second = Math.max(first + nums.get(i), second);
+            first = temp;
+        }
+        return second;
+    }
+}
