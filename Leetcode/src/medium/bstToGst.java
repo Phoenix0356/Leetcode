0406aa054ed3b15f9fddbd966b918490e6304fe6
Phoenix0356 <1242780667@qq.com>
2023-12-04 01:14:41 +0800

    20231204

---
+++ Leetcode/src/medium/bstToGst.java
@@ -0,0 +1,16 @@
+package medium;
+
+import util.TreeNode;
+
+public class bstToGst {
+    int sum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if (root != null) {
+            bstToGst(root.right);
+            sum += root.val;
+            root.val = sum;
+            bstToGst(root.left);
+        }
+        return root;
+    }
+}
